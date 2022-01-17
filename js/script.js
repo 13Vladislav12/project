dd11f288248cb712b6e588106dded29e86546719
Vladislav <example@gmail.com>
2022-01-17 21:05:52 +0300

    second

---
+++ js/script.js
@@ -0,0 +1,41 @@
+let number=5;
+const leftBorderWidth =1;
+number =10;
+console.log(number);
+
+const obj = {
+    a: 1312
+};
+
+obj.a = 1213;
+
+
+console.log("string" * 9);
+console.log(something);
+let und;
+console.log(und);
+
+
+
+alert("Hello");
+alert("Hello");
+const result =confirm ("Are you men?");
+
+
+console.log("arr" + "- object");
+console.log(4 + "- object");
+let incr = 10,
+    decr = 10;
+    incr++;
+    decr--;
+console.log(incr);
+console.log(decr);
+console.log(4*8 ==="32");
+//const isCheked =true,
+      //isClose =false;
+     // console.log(isCheked && isClose);
+
+      const isCheked = true,
+            isClose =true;
+      console.log(isCheked || isClose);
+      
